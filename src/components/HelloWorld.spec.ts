94bf524348d569e8931063e1bcffc0502ebb97b4
Rogerio Pereira <hi@rogeriopereira.dev>
2024-09-04 18:28:37 -0400

    4. @vue/unit-test VS testing-library/vue

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -1,5 +1,5 @@
 import { expect, it } from "vitest"
-import { mount } from "@vue/test-utils"
+import { mount, shallowMount } from "@vue/test-utils"
 import HelloWorld from "./HelloWorld.vue"
 
 it('should render message from props', () => {
@@ -11,4 +11,27 @@ it('should render message from props', () => {
 
     // expect(instance.find('h1').html()).toContain('My first test!')
     expect(instance.find('h1').text()).toBe("My first test!")
+})
+
+// WHITEBOX approach, you test the methods internally
+it('should increment count when increment method is called', () => {
+    const instance = mount(HelloWorld)
+
+    expect(instance.vm.count).toBe(0)
+
+    instance.vm.increment()
+    expect(instance.vm.count).toBe(1)
+})
+
+// BLACKBOX approach, you test the behaviors not the methods
+it('should increment count and display it', async () => {
+    const instance = mount(HelloWorld)
+
+    const button = instance.find('button')
+
+    expect(button.text()).toBe('count is 0')
+
+    await button.trigger('click')
+
+    expect(button.text()).toBe('count is 1')
 })
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,17 @@ defineProps({
 })
 
 const count = ref(0)
+
+const increment = () => {
+    count.value++
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
